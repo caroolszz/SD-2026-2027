eaae9690f856a381f06a13c5252f67f2507e9a9c
Clementina Couto <pv28327@alunos.estgv.ipv.pt>
2026-09-24 00:22:58 +0100

    E1-UDP01 4.2: servidor com estado L e waitingfor

--- Sprints/E1-UDP01/udp01-servidor/src/UDPServer.java
+++ Sprints/E1-UDP01/udp01-servidor/src/UDPServer.java
@@ -9,17 +9,39 @@ public class UDPServer {
         try {
             aSocket = new DatagramSocket(6789);
             byte[] buffer = new byte[1000];
+            int L = 0;
 
             while (true) {
                 DatagramPacket request = new DatagramPacket(buffer, buffer.length);
                 aSocket.receive(request);
 
-                DatagramPacket reply = new DatagramPacket(request.getData(),
-                        request.getLength(), request.getAddress(), request.getPort());
+                String mensagem = new String(request.getData(), 0, request.getLength());
+
+                int N = -1;
+                int virgula = mensagem.indexOf(',');
+                if (virgula > 0) {
+                    try {
+                        N = Integer.parseInt(mensagem.substring(0, virgula));
+                    } catch (NumberFormatException e) {
+                        N = -1;
+                    }
+                }
+
+                String resposta;
+                if (N == L + 1) {
+                    resposta = mensagem;
+                    L = N;
+                } else {
+                    resposta = "waitingfor," + (L + 1);
+                }
+
+                byte[] r = resposta.getBytes();
+                DatagramPacket reply = new DatagramPacket(r, r.length,
+                        request.getAddress(), request.getPort());
 
                 aSocket.send(reply);
                 System.out.println("Recebido de " + request.getAddress() + ":" + request.getPort()
-                        + " -> " + new String(request.getData(), 0, request.getLength()));
+                        + " -> " + mensagem + " | resposta: " + resposta + " | L = " + L);
             }
         } catch (SocketException e) { System.out.println("Socket: " + e.getMessage());
         } catch (IOException e)     { System.out.println("IO: " + e.getMessage());
